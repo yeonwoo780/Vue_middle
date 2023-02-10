662970f5985bc99367cdaeb04f966a401aeb8c10
yeonwoo <rlckrl0315db@gmail.com>
2023-02-10 18:07:14 +0900

    0210 학습분

--- vue-todo/src/components/TodoList.vue
+++ vue-todo/src/components/TodoList.vue
@@ -1,10 +1,12 @@
 <template>
   <div>
     <ul>
-      <li v-for="(todoItem, index) in todoItems" v-bind:key="todoItem" class="shadow">
-        {{ todoItem }}
+      <li v-for="(todoItem, index) in propsdata" v-bind:key="todoItem.item" class="shadow">
+        <i class="checkBtn fas fa-check-square" v-bind:class="{checkBtnCompleted: todoItem.completed}" 
+          v-on:click="toggleComplete(todoItem, index)"></i>
+        <span v-bind:class="{textCompleted: todoItem.completed}">{{ todoItem.item }}</span>
         <span class="removeBtn" v-on:click="removeTodo(todoItem, index)">
-          del
+          <i class="fas fa-trash"></i>
         </span>
       </li>
     </ul>
@@ -13,24 +15,18 @@
 
 <script>
 export default {
-  data: function(){
-    return{
-      todoItems: []
-    }
-  },
+  props: ["propsdata"],
   methods: {
     removeTodo: function(todoItem, index) {
       console.log(todoItem, index);
       localStorage.removeItem(todoItem);
       this.todoItems.splice(index, 1);
-    }
-  },
-  created: function() {
-    if (localStorage.length > 0) {
-      for (var i = 0; i < localStorage.length; i++) {
-        if (localStorage.key(i) !== "loglevel:webpack-dev-server")
-        this.todoItems.push(localStorage.key(i))
-      }
+    },
+    toggleComplete: function(todoItem) {
+      // console.log(todoItem, index);
+      todoItem.completed = !todoItem.completed;
+      localStorage.removeItem(todoItem.item);
+      localStorage.setItem(todoItem.item, JSON.stringify(todoItem));
     }
   }
 }
